17f0dbc724c1f0dd572de069dbaeb1bea3e1f024
Angotti AI Dev <angottiai@proton.me>
2025-06-24 23:56:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ function changeMessage() {
   const message = document.getElementById("message");
   message.innerText = "You just ran your first JavaScript function! 🚀";
 }
+
+let count = 0;
+
+function incrementCount() {
+  count += 1;
+  document.getElementById("count").innerText = count;
+}
